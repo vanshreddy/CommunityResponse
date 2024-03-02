613f5b95d897054d24e0723ebcc1152e94923832
Vansh Reddy <68335743+vanshreddy@users.noreply.github.com>
2024-03-01 19:04:17 -0500

    created connection to sql using sqllite

--- Backend/utils/db.py
+++ Backend/utils/db.py
@@ -1,37 +1,25 @@
-from pymongo import MongoClient, errors
+import sqlite3
+from sqlite3 import Error
 
-def create_connection(host, port, username, password, db_name):
-    client = None
+def create_connection(db_file):
+    """Create a database connection to the SQLite database specified by db_file"""
+    conn = None
     try:
-        # Create a MongoDB client
-        client = MongoClient(host=host, port=port, username=username, password=password, authSource=db_name)
+        conn = sqlite3.connect(db_file)
+        print("Connection is established: Database is created in memory")
+    except Error as e:
+        print(e)
+    return conn
 
-        client.server_info() 
-        print("MongoDB connection successful")
-    except errors.ServerSelectionTimeoutError as err:
-        # If connection is not successful, print the error
-        print("PyMongo ERROR:", err)
-        client = None
-    except Exception as e:
-        print("An error occurred:", e)
-        client = None
-
-    return client
-
-def get_database(client, db_name):
-    db = None
+def create_table(conn, create_table_sql):
+    """Create a table from the create_table_sql statement"""
     try:
-        db = client[db_name]
-    except Exception as e:
-        print(f"An error occurred while getting the database: {e}")
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
 
-    return db
 
-def get_collection(db, collection_name):
-    collection = None
-    try:
-        collection = db[collection_name]
-    except Exception as e:
-        print(f"An error occurred while getting the collection: {e}")
-
-    return collection
+def close_connection(conn):
+    """Close database connection"""
+    conn.close()
